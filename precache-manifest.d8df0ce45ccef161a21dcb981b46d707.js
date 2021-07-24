d90cda4e1b450e3f4fc1a519a2897a0ee73d9a28
ElmisteriOkay <walkpeting@gmail.com>
2021-07-24 14:18:35 -0500

    Updates

---
+++ precache-manifest.d8df0ce45ccef161a21dcb981b46d707.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf8d5a825ff27cf9f7e9161d006eaeb",
+    "url": "/clima-app/index.html"
+  },
+  {
+    "revision": "d7c9d450c8da4077306a",
+    "url": "/clima-app/static/css/main.7ce843e4.chunk.css"
+  },
+  {
+    "revision": "ac6b69b194f9e8696bf0",
+    "url": "/clima-app/static/js/2.f6402436.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clima-app/static/js/2.f6402436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7c9d450c8da4077306a",
+    "url": "/clima-app/static/js/main.eaacafd3.chunk.js"
+  },
+  {
+    "revision": "8130b3243f664ea6ace1",
+    "url": "/clima-app/static/js/runtime-main.761349cd.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/clima-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/clima-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
